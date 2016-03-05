1308b495b92e5ae8b9de26b69a21d6e772bdc0e3
Big <channimit@gmail.com>
2016-03-06 01:47:03 +0700

    read node

--- app/src/main/java/com/jolmagic/channimit/blognonefast/view/NewsHeadlineAdapter.java
+++ app/src/main/java/com/jolmagic/channimit/blognonefast/view/NewsHeadlineAdapter.java
@@ -1,5 +1,6 @@
 package com.jolmagic.channimit.blognonefast.view;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.einmalfel.earl.Item;
+import com.jolmagic.channimit.blognonefast.NodeScrollingActivity;
 import com.jolmagic.channimit.blognonefast.R;
 
 import java.util.List;
@@ -25,15 +27,27 @@ public class NewsHeadlineAdapter extends RecyclerView.Adapter<NewsHeadlineAdapte
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
         public TextView headlineView;
         public TextView descView;
-        public ViewHolder(View convertView) {
-            super(convertView);
+        public IMyViewHolderClicks mListener;
 
+        public ViewHolder(View convertView, IMyViewHolderClicks iMyViewHolderClicks) {
+            super(convertView);
+            mListener = iMyViewHolderClicks;
+            convertView.setOnClickListener(this);
             headlineView  = (TextView) convertView.findViewById(R.id.headline_in_view_headline);
             descView = (TextView) convertView.findViewById(R.id.desc_in_view_headline);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v != null) mListener.onClick(v, getAdapterPosition(), this);
+        }
+
+        public interface IMyViewHolderClicks {
+            void onClick(View caller, int position, ViewHolder viewHolder);
         }
     }
 
@@ -48,8 +62,21 @@ public class NewsHeadlineAdapter extends RecyclerView.Adapter<NewsHeadlineAdapte
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.view_headline, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        return new ViewHolder(v);
+
+        return new ViewHolder(v, new ViewHolder.IMyViewHolderClicks() {
+            @Override
+            public void onClick(View caller, int position, ViewHolder viewHolder) {
+                Item news = mDataset.get(position);
+                Intent intent = new Intent(caller.getContext(), NodeScrollingActivity.class);
+                intent.putExtra("Title", news.getTitle());
+                intent.putExtra("Description", news.getDescription());
+                intent.putExtra("Author", news.getAuthor());
+                intent.putExtra("ImageLin", news.getImageLink());
+                intent.putExtra("Link", news.getLink());
+                intent.putExtra("PublicationDate", news.getPublicationDate());
+                caller.getContext().startActivity(intent);
+            }
+        });
     }
 
     // Replace the contents of a view (invoked by the layout manager)
